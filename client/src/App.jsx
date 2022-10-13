b01137ce20fbeb458e98960cb864a6b207afdd38
jinonensk <jinonensk@gmail.com>
2022-10-14 04:58:37 +0700

    make uniq url

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,53 +1,41 @@
-// import {
-//   createBrowserRouter,
-//   createRoutesFromElements,
-//   Route,
-// } from 'react-router-dom'
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  redirect,
+  Route,
+  RouterProvider,
+} from 'react-router-dom'
 import './styles/app.scss'
 import Canvas from './compoentns/Canvas'
 import SettingBar from './compoentns/SettingBar'
 import Toolbar from './compoentns/Toolbar'
 
-// import RootLayout from './layout/RootLayout'
-
-// const router = createBrowserRouter(
-//   createRoutesFromElements(
-//     <Route path="/" element={<RootLayout />}>
-//       <Route path="/:id">
-//         <Toolbar />
-//         <SettingBar />
-//         <Canvas />
-//       </Route>
-//     </Route>,
-//     // <Route path="/" element={<RootLayout/>}>
-//     //   <Route path="/:id">
-//     //     <Toolbar />
-//     //     <SettingBar />
-//     //     <Canvas />
-//     //   </Route>
-//     // </Route>,
-//   ),
-// )
+import RootLayout from './layout/RootLayout'
 
 function App() {
   return (
-    <div className="app">
-      <Toolbar />
-      <SettingBar />
-      <Canvas />
-    </div>
-    // <RouterProvider router={router}
-
-    // <div className="app">
-    //   <Switch>
-    //     <Route path="/:id">
-    //       <Toolbar />
-    //       <SettingBar />
-    //       <Canvas />
-    //     </Route>
-    //     <Redirect to={`f${(+new Date()).toString(16)}`} />
-    //   </Switch>
-    // </div>
+    <RouterProvider
+      router={createBrowserRouter(
+        createRoutesFromElements(
+          <Route path="/" element={<RootLayout />}>
+            <Route
+              index
+              loader={() => redirect(`/f${new Date().getTime().toString(16)}`)}
+            />
+            <Route
+              path="/:id"
+              element={
+                <>
+                  <Toolbar />
+                  <SettingBar />
+                  <Canvas />
+                </>
+              }
+            ></Route>
+          </Route>,
+        ),
+      )}
+    />
   )
 }
 
